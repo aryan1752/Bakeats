068a30286557e214ad919612ad82d31a7e5520b4
aryan1752 <jharozy24@gmail.com>
2026-03-04 20:55:25 +0530

    Reduce extra space below parallax on mobile

--- app/components/ui/hero-parallax.tsx
+++ app/components/ui/hero-parallax.tsx
@@ -44,7 +44,7 @@ export const HeroParallax = ({ products }: { products: Product[] }) => {
   return (
     <div
       ref={ref}
-      className="h-[300vh] py-24 md:h-[300vh] md:py-40 overflow-hidden bg-black antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[220vh] py-16 md:h-[300vh] md:py-40 overflow-hidden bg-black antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div style={{ rotateX, rotateZ, translateY, opacity }}>
